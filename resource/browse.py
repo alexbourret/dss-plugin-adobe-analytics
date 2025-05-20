10a0f7b735db16b3bbb4e7ef023184d0fec61df1
Alex Bourret <alexandre.bourret@dataiku.com>
2025-05-20 11:14:26 +0200

    Adding dynamic UI

---
+++ resource/browse.py
@@ -0,0 +1,73 @@
+from safe_logger import SafeLogger
+from adobe_analytics_common import (
+    get_connection_from_config
+)
+from adobe_client import AdobeClient
+from dss_selector_choices import DSSSelectorChoices, get_value_from_ui
+
+
+logger = SafeLogger("adobe-analytics browser", ["bearer_token", "api_key", "client_secret"])
+mock = False
+
+
+def do(payload, config, plugin_config, inputs):
+    logger.info("do:payload={}, config={}, plugin_config={}, inputs={}".format(logger.filter_secrets(payload), logger.filter_secrets(config), plugin_config, inputs))
+    choices = DSSSelectorChoices()
+
+    try:
+        organization_id, company_id, api_key, bearer_token = get_connection_from_config(config, mock=mock)
+    except Exception as error:
+        logger.error("Error while getting token : {}".format(error))
+        return choices.text_message("Error: Please check the logs.")
+
+    logger.info("organization_id={}, company_id={}, api_key={}".format(
+            organization_id, company_id, api_key
+        )
+    )
+    parameter_name = payload.get('parameterName')
+
+    if not bearer_token:
+        print("ALX:not bearertoken")
+        if parameter_name == "report_id":
+            return choices.text_message("Set the authentication")
+        else:
+            return choices.to_dss()
+
+    try:
+        client = AdobeClient(
+            company_id=company_id,
+            api_key=api_key,
+            access_token=bearer_token,
+            organization_id=organization_id,
+            mock=mock
+        )
+        if parameter_name == "report_id":
+            for report_suite in client.next_report_suites():
+                label = report_suite.get("name")
+                value = report_suite.get("rsid")
+                if label and value:
+                    choices.append(label, value)
+            choices.append_manual_select()
+        elif parameter_name == "metrics_ids":
+            report_id = get_value_from_ui(payload, "report_id")
+            print("listing metrics for rsid '{}'".format(report_id))
+            if report_id:
+                for metric in client.next_metric(report_id):
+                    label = metric.get("name")
+                    value = metric.get("id")
+                    if label and value:
+                        choices.append(label, value)
+        elif parameter_name == "dimension":
+            report_id = get_value_from_ui(payload, "report_id")
+            logger.info("listing dimensions for rsid '{}'".format(report_id))
+            if report_id:
+                for dimension in client.next_dimension(report_id):
+                    label = dimension.get("name")
+                    value = dimension.get("id")
+                    if label and value:
+                        choices.append(label, value)
+            choices.append_manual_select()
+    except Exception as error:
+        logger.error("Error while fetching {} : {}".format(parameter_name, error))
+        return choices.text_message("Error: Please check the logs")
+    return choices.to_dss()
