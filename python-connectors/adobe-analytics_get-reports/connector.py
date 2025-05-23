a53e4b465a15de3e5416f8d7ab8e6a0552244ca1
Alex Bourret <alexandre.bourret@dataiku.com>
2025-05-23 17:48:20 +0200

    merging

--- python-connectors/adobe-analytics_get-reports/connector.py
+++ python-connectors/adobe-analytics_get-reports/connector.py
@@ -80,6 +80,7 @@ class AdobeAnalyticsConnector(Connector):
 
         # self.dimension = self.config.get("dimension")
         self.dimension = get_value_from_ui(self.config, "dimension")
+        self.segment = get_value_from_ui(self.config, "segment")
         auth_type = config.get("auth_type", "user_account")
         logger.info("auth_type={}".format(auth_type))
         user_account = config.get(auth_type, {})
@@ -135,6 +136,34 @@ class AdobeAnalyticsConnector(Connector):
         except Exception as error:
             logger.error("Error {} while listing report dimensions".format(error))
 
+        logger.info("Testing pagination on report_suites...")
+        try:
+            report_suites = self.client.list_report_suites()
+            logger.info("report_suites={}".format(report_suites))
+        except Exception as error:
+            logger.error("Error {} while listing report suites".format(error))
+
+        logger.info("Testing pagination on metrics for {}...".format(self.report_id))
+        try:
+            report_metrics = self.client.list_report_metrics(self.report_id)
+            logger.info("report_metrics={}".format(report_metrics))
+        except Exception as error:
+            logger.error("Error {} while listing report metrics".format(error))
+
+        logger.info("Testing pagination on dimensions for {}...".format(self.report_id))
+        try:
+            report_dimensions = self.client.list_report_dimensions(self.report_id)
+            logger.info("report_metrics={}".format(report_dimensions))
+        except Exception as error:
+            logger.error("Error {} while listing report dimensions".format(error))
+
+        logger.info("Testing pagination on segments for {}...".format(self.report_id))
+        try:
+            report_segments = self.client.list_report_segments(self.report_id)
+            logger.info("report_segments={}".format(report_segments))
+        except Exception as error:
+            logger.error("Error {} while listing report segments".format(error))
+
     def get_read_schema(self):
         """
         Returns the schema that this connector generates when returning rows.
@@ -169,7 +198,8 @@ class AdobeAnalyticsConnector(Connector):
             start_date=self.start_date,
             end_date=self.end_date,
             metrics=self.metrics,
-            dimension=self.dimension
+            dimension=self.dimension,
+            segment=self.segment
         )
         logger.info("json_response={}".format(json_response))
         rows = reorder_response(json_response, self.metrics_names)
--- resource/browse.py
+++ resource/browse.py
@@ -53,7 +53,9 @@ def do(payload, config, plugin_config, inputs):
             print("listing metrics for rsid '{}'".format(report_id))
             if report_id:
                 for metric in client.next_metric(report_id):
-                    label = metric.get("name")
+                    # Metric's labels are not unique, but multiselect cannot stand that
+                    # so the metric ID is added to the label
+                    label = "{} - {}".format(metric.get("name"), metric.get("id"))
                     value = metric.get("id")
                     if label and value:
                         choices.append(label, value)
@@ -66,6 +68,15 @@ def do(payload, config, plugin_config, inputs):
                     value = dimension.get("id")
                     if label and value:
                         choices.append(label, value)
+            choices.append_manual_select()
+        elif parameter_name == "segment":
+            report_id = get_value_from_ui(payload, "report_id")
+            if report_id:
+                for segment in client.next_segment(report_id):
+                    label = segment.get("name")
+                    value = segment.get("id")
+                    if label and value:
+                        choices.append_alphabetically(label, value)
             choices.append_manual_select()
     except Exception as error:
         logger.error("Error while fetching {} : {}".format(parameter_name, error))
