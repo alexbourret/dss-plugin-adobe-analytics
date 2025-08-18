db3f3ba7817469af1ef4a2deb6210b1901ef86f4
Alex Bourret <alexandre.bourret@dataiku.com>
2025-08-18 11:00:55 +0200

    bump v0.0.16

--- python-connectors/adobe-analytics_get-reports/connector.py
+++ python-connectors/adobe-analytics_get-reports/connector.py
@@ -17,7 +17,7 @@ class AdobeAnalyticsConnector(Connector):
     def __init__(self, config, plugin_config):
         Connector.__init__(self, config, plugin_config)
         logger.info(
-            "Starting plugin adobe-analytics v0.0.15 with config={}".format(
+            "Starting plugin adobe-analytics v0.0.16 with config={}".format(
                 logger.filter_secrets(config)
             )
         )
