3a5e3f22b4a46f0de74d96aa2a19a3accb798247
Alex Bourret <alexandre.bourret@dataiku.com>
2025-09-22 17:50:36 +0200

    v0.1.0

--- python-connectors/adobe-analytics_get-reports/connector.py
+++ python-connectors/adobe-analytics_get-reports/connector.py
@@ -16,7 +16,7 @@ class AdobeAnalyticsConnector(Connector):
     def __init__(self, config, plugin_config):
         Connector.__init__(self, config, plugin_config)
         logger.info(
-            "Starting plugin adobe-analytics v0.0.19 with config={}".format(
+            "Starting plugin adobe-analytics v0.1.0 with config={}".format(
                 logger.filter_secrets(config)
             )
         )
